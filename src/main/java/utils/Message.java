e7410ed7b4873c9595ca05a75e98f1b4bede82ff
moo1o <kmyms2@naver.com>
2021-05-04 16:36:51 +0900

    fix : update message

--- src/main/java/utils/Message.java
+++ src/main/java/utils/Message.java
@@ -3,7 +3,7 @@ package utils;
 public enum Message {
     ASK_NAME("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분\n"),
     ASK_TIMES("시도할 횟수는 몇회인가요?\n"),
-    START("실행 결과\n"),
+    START("\n실행 결과\n"),
     RESULT("가 최종 우승했습니다.\n");
 
     final private String message;
