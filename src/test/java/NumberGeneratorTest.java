686ffcb3857593a57ae1c5dde39e10d5cfee8e96
moo1o <kmyms2@naver.com>
2021-05-04 10:29:34 +0900

    test : add NumberGeneratorTest
    - 숫자 생성기 테스트

---
+++ src/test/java/NumberGeneratorTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import utils.NumberGenerator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class NumberGeneratorTest {
+    @Test
+    public void numberGenerateTest(){
+        for(int i=0 ; i<100 ; i++){
+            String input = Integer.toString(NumberGenerator.numberGenerate());
+            System.out.println(input);
+            assertThat(input).matches("[0-9]{1}");
+        }
+    }
+}
