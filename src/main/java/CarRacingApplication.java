9666cf4a819f503eab48da20b508d3a5d5a0fe4b
moo1o <kmyms2@naver.com>
2021-05-04 16:37:33 +0900

    feat : add CarRacingApplication
    - 레이싱 수행 어플리케이션 생성

---
+++ src/main/java/CarRacingApplication.java
@@ -0,0 +1,35 @@
+import car.Car;
+import car.Cars;
+import race.Racing;
+import utils.InputApi;
+import utils.Message;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarRacingApplication {
+    public static void main(String[] args) {
+        List<Car> carList = new ArrayList<>();
+        Cars cars;
+        System.out.print(Message.ASK_NAME.getMessage());
+        String input = InputApi.input();
+        String[] names = input.split(",");
+        // 이름 앞뒤 공백 제거 후 자동차 객체 생성
+        for(int i=0 ; i<names.length ; i++){
+            names[i] = names[i].trim();
+            carList.add(new Car(names[i]));
+        }
+        cars = new Cars(carList);
+        System.out.print(Message.ASK_TIMES.getMessage());
+        int times = Integer.parseInt(InputApi.input());
+
+        System.out.print(Message.START.getMessage());
+        for(int i=0 ; i<times ; i++){
+            Racing.racingCars(cars);
+        }
+        cars.winnerCheck();
+        cars.printWinner();
+    }
+
+
+}
