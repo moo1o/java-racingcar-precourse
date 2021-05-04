2b6632415d42cfa3850031b3e653c6d85f823950
moo1o <kmyms2@naver.com>
2021-05-04 14:29:24 +0900

    feat : add Cars class function
    - carList 반환 기능 추가
    - 우승자 체크 후 우승자 리스트 반환

--- src/main/java/car/Cars.java
+++ src/main/java/car/Cars.java
@@ -2,13 +2,23 @@ package car;
 
 import utils.NumberGenerator;
 
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Cars {
     List<Car> carList;
+    List<Car> winnerCars;
 
     public Cars(List<Car> carList) {
         this.carList = carList;
+    }
+
+    /*
+        car list 반환
+     */
+    public List<Car> getCarList(){
+        return carList;
     }
 
     /*
@@ -37,5 +47,29 @@ public class Cars {
             car.printCar();
         }
         System.out.println();
+    }
+
+    /*
+        우승자 체크
+     */
+    public List<Car> winnerCheck(){
+        int winnerDistance = 0;
+        winnerCars = new ArrayList<>();
+        for(Car car : carList){
+            winnerDistance = Math.max(winnerDistance, car.getDistance());
+        }
+        for(Car car : carList) {
+            addWinner(car, winnerDistance);
+        }
+        return winnerCars;
+    }
+
+    /*
+        우승자 추가
+     */
+    private void addWinner(Car car, int winnerDistance){
+        if(car.getDistance() == winnerDistance){
+            winnerCars.add(car);
+        }
     }
 }
